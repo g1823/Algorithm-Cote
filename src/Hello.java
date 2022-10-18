626c731a07a1727011cbb76d0c04c46e956cfb6b
GaoJie <g18235788244@163.com>
2022-10-18 16:08:23 +0800

    Frist commit

---
+++ src/Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+    int a;
+    String b;
+
+    public Hello(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+//        System.out.println("你好");
+        int a = 0;
+        Object obj = new Object();
+        obj.equals(1);
+        Integer i = new Integer(20);
+        Integer i2 = new Integer(10);
+        i.equals(i2);
+    }
+}
