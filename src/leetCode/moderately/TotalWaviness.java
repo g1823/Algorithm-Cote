702bdb3451d6bc9c59901acfcfba84f57b2f85dd
gj <2418728171@qq.com>
2026-06-12 16:50:03 +0800

    1

---
+++ src/leetCode/moderately/TotalWaviness.java
@@ -0,0 +1,10 @@
+package leetCode.moderately;
+
+/**
+ * @description: 3751. 范围内总波动值 I todo
+ */
+public class TotalWaviness {
+    public int totalWaviness(int num1, int num2) {
+        return 0;
+    }
+}
